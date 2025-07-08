a82d37416cced0f04baef51f22138ee5f0bad63a
Raj Kishor <razzzz87@gmail.com>
2025-07-08 22:23:27 +0530

    Added Controls in file processing tab, And disabled other files code, as it was not completed so throwing error

--- TargetEchoGen/matfileprocessing.h
+++ TargetEchoGen/matfileprocessing.h
@@ -1,42 +1,42 @@
-#ifndef MATFILEPROCESSING_H
-#define MATFILEPROCESSING_H
+// #ifndef MATFILEPROCESSING_H
+// #define MATFILEPROCESSING_H
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <thread>
-#include <chrono>
-#include <atomic>
-#include <cmath>
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <thread>
+// #include <chrono>
+// #include <atomic>
+// #include <cmath>
 
-class RadarSession {
-public:
-    RadarSession();
-    void startSession();
-    void stopSession();
-    void runTimer();
+// class RadarSession {
+// public:
+//     RadarSession();
+//     void startSession();
+//     void stopSession();
+//     void runTimer();
 
-private:
-    void handleInterrupt();
-    void loadData();
-    void processMotionData();
-    void updatePlatformAttitude();
-    void updatePulseAmplitude();
-    void process_file_data();
+// private:
+//     void handleInterrupt();
+//     void loadData();
+//     void processMotionData();
+//     void updatePlatformAttitude();
+//     void updatePulseAmplitude();
+//     void process_file_data();
 
 
-    const double speed_light = 299792458;
-    std::atomic<bool> sessionDoesNotEnd;
-    std::atomic<int> counter;
-    double PRT;
-    double RTX, RTY, RTZ;
-    double RTYaw;
+//     const double speed_light = 299792458;
+//     std::atomic<bool> sessionDoesNotEnd;
+//     std::atomic<int> counter;
+//     double PRT;
+//     double RTX, RTY, RTZ;
+//     double RTYaw;
 
-    std::string input_sourceofplatformattitudedata; // Defined here
-    bool RadarLookAngleDataavlonNetwork; // Assuming this is a boolean
+//     std::string input_sourceofplatformattitudedata; // Defined here
+//     bool RadarLookAngleDataavlonNetwork; // Assuming this is a boolean
 
-    std::chrono::microseconds period;
-    std::thread timerThread;
-};
+//     std::chrono::microseconds period;
+//     std::thread timerThread;
+// };
 
-#endif // MATFILEPROCESSING_H
+// #endif // MATFILEPROCESSING_H
